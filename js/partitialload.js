f89bb43a469b7293d740b0502304400f4b07b030
nfast <nathalie.fast@student.uni-luebeck.de>
2021-01-19 11:14:37 +0100

    added some js for page loading

---
+++ js/partitialload.js
@@ -0,0 +1,21 @@
+
+function initializePartitialLoad(){
+    $(".partitial-load").unbind("click");
+
+    $(".partitial-load").bind("click", function(event){
+        let element = this;
+        let target_div = $(element).data("target");
+        let partitial_url = $(element).data("href");
+        
+        $.get(partitial_url, function (data){
+            $(target_div).html(data);
+        })
+        
+    });
+    
+}
+
+
+window.onload = function(){
+    initializePartitialLoad();
+}

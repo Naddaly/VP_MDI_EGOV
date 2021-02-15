f309bbe0f990f59abcbedcda1ce306b09a8eed43
nfast <nathalie.fast@student.uni-luebeck.de>
2021-02-15 14:05:46 +0100

    Website Rebuild for Evaluation

---
+++ js/autofill.js
@@ -0,0 +1,25 @@
+let filldata = {
+    "gemeinde": "Lübeck",
+    "first_name": "Max",
+    "last_name": "Mustermann",
+    "birth_name": "",
+    "birth_date": "01.01.2000",
+    "birth_place": "Lübeck",
+    "citizenship": "Deutschland",
+    "family_status": "ledig",
+    "gender": "männlich",
+    "phone_number": "0123456789",
+    "occupation": "Student"
+};
+
+function initalizeAutofill(){
+    
+    $("input.fillme").bind("click", function(){
+        $(this).val( filldata[ $(this).attr("name") ]   )
+    });
+    
+}
+
+$( document ).ready(function() {
+    initalizeAutofill();
+});
--- js/partitialload.js
+++ js/partitialload.js
@@ -3,22 +3,76 @@ function initializePartitialLoad(){
 
     $(".partitial-load").bind("click", function(event){
         let element = this;
+        let missing_fields = [];
+        
         let target_div = $(element).data("target");
         let partitial_url = $(element).data("href");
         let nav_item = $(element).data("nav");
         
-        if(typeof(nav_item) != "undefined"){
-            window.location.hash = nav_item;
+        if(!$(this).hasClass("disable-require")){
+            $("input[type=text].required").each(function(){
+                if($(this).val().trim() == ""){
+                    missing_fields.push("Bitte das Feld "+$(this).data("required-name")+" ausfüllen!")
+                }
+        
+            });
+    
+            let required_radios = [];
+            $("input[type=radio].required").each(function() {
+        
+                let radioName = $(this).attr("name");
+        
+                if (!required_radios.includes(radioName)) {
+                    required_radios.push(radioName);
+                }
+            });
+    
+            $(required_radios).each(function(index, radioName){
+                let val = $("input[name="+radioName+"]:checked").val();
+        
+                if(val == undefined || val.trim() == ""){
+                    missing_fields.push("Bitte das Feld "+$("input[name="+radioName+"]").data("required-name")+" angeben!")
+                }
+        
+            });
+    
         }
         
-        $.get(partitial_url, function (data){
-            $(target_div).html(data);
-        })
+       
+        
+    
+        if(missing_fields.length > 0){
+            alert(missing_fields.join("\n"))
+        }else{
+            
+            if($(this).data("alert") !== undefined){
+                alert($(this).data("alert"));
+            }
+            if($(this).data("confirm") !== undefined){
+                let res = confirm($(this).data("confirm"));
+                if(!res){
+                    return false;
+                }
+            }
+            
+            if(typeof(nav_item) != "undefined"){
+                window.location.hash = nav_item;
+            }
+    
+            if(target_div == "_blank"){
+                window.location.href = partitial_url;
+            }else{
+                $.get(partitial_url, function (data){
+                    $(target_div).html(data);
+                })
+            }
+            
+        }
         
     });
     
 }
 
-window.onload = function(){
+$( document ).ready(function() {
     initializePartitialLoad();
-}
+});
